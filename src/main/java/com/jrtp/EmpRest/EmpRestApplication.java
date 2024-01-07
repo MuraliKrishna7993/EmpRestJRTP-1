11ef402b0b5f9ac62b984e77d3cffdfec5b76638
MuraliKrishna7993 <muralimsdian1997@gmail.com>
2024-01-07 10:59:51 +0530

    first commit

---
+++ src/main/java/com/jrtp/EmpRest/EmpRestApplication.java
@@ -0,0 +1,13 @@
+package com.jrtp.EmpRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrtp/EmpRest/EmpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrtp.EmpRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
